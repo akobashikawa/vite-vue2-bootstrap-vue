2dd6a8d19f4ed4154786a14fb7772580cd8f5dc8
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-07-09 14:16:00 -0500

    Configura vue2 y bootstrap-vue

--- README.md
+++ README.md
@@ -6,4 +6,54 @@
 - `npm run dev`
 - http://localhost:5173/
 
-- 
+- `npm install --save vue@2`
+- `npm uninstall --save @vitejs/plugin-vue vue@3`
+- `npm install --save  @vitejs/plugin-vue2`
+- `npm install --save vue-router@3.5.1`
+- `npm install --save bootstrap-vue bootstrap@4.3.1`
+
+```js
+// vite.config.js
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+```
+
+```js
+// src\main.js
+
+// import { createApp } from 'vue'
+import Vue from "vue";
+import router from "./router";
+import './style.css'
+import App from './App.vue'
+
+/*--------------------REGISTER BOOTSTRAP---------------------------------*/
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+/*-----------------------------------------------------------------------*/
+
+// createApp(App).mount('#app')
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
+
+```
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,38 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="hello">
+    <b-jumbotron
+      header="Vue2 Bootstrap Vue"
+      lead="Bootstrap v4 Components for Vue.js 2"
+    >
+      <p>{{ msg }}</p>
+      <b-button variant="primary" @click="helloworld">Hello World</b-button>
+    </b-jumbotron>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <b-container fluid>
+      <h1>{{ message }}</h1>
+    </b-container>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    helloworld() {
+      this.message = "Hello World";
+      this.$bvModal.msgBoxOk(this.message);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
